7ee73926ace36f328c63fe7d65f5960cbb5377f2
mehmetsayim <108213148+mehmetsayim@users.noreply.github.com>
2022-12-04 12:37:13 +0300

    Delete Html denemesi directory

--- Html denemesi/Html denemesi/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Html_denemesi.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
